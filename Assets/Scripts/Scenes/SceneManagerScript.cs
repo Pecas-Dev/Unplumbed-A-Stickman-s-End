d435559bb3c41baa4d6cd721f0db0a49f8238b58
PecasDev <juangc1004@hotmail.com>
2023-02-14 21:51:14 -0500

    Minor Fixes and Updates (Prototype)

--- Assets/Scripts/Scenes/SceneManagerScript.cs
+++ Assets/Scripts/Scenes/SceneManagerScript.cs
@@ -36,10 +36,24 @@ public class SceneManagerScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && gameObject.tag == "Square1")
         {
             StartCoroutine(Loading1());
         }
+
+        if (collision.gameObject.tag == "Player" && gameObject.tag == "Square2")
+        {
+            StartCoroutine(ToLevel1());
+        }
+    }
+
+    IEnumerator ToLevel1()
+    {
+        transitionAnimator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene("Level1");
     }
 
     IEnumerator Loading1()
