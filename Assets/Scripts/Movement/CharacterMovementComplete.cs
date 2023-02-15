dcb82d7d85188edf539b98dd891cc58f7da1a6ef
PecasDev <juangc1004@hotmail.com>
2023-02-15 15:27:24 -0500

    Key Prototype Mechanics Completed

--- Assets/Scripts/Movement/CharacterMovementComplete.cs
+++ Assets/Scripts/Movement/CharacterMovementComplete.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CharacterMovementComplete : MonoBehaviour
 {
+    [Header("Speed")]
     [SerializeField] float playerSpeed = 10f;
-    [SerializeField] float firstJumpForce = 5f;
-    [SerializeField] float secondJumpForce = 2f;
+    //[SerializeField] float firstJumpForce = 5f;
+    //[SerializeField] float secondJumpForce = 2f;
     [SerializeField] float walkAnimationSpeed = 1f;
-    //[SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    [Header("Wall")]
+    [SerializeField] Transform wallCheck;
+    [SerializeField] LayerMask wallLayer;
 
 
     Rigidbody2D playerCompleteRigidody;
@@ -21,8 +26,21 @@ public class CharacterMovementComplete : MonoBehaviour
     float xInputValue;
     float yInputValue;
 
+    //They don't do nothing, but somehow my code works with them /:
     bool hasJumped;
     bool hasWallJumped;
+
+    Vector2 secondJumpPower = new Vector2(0f, 0f);
+
+    float wallSlidngSpeed = 2f;
+    bool isWallSliding;
+
+    Vector2 wallJumpingPower = new Vector2(10f, 8f);
+    bool iswallJumping;
+    float wallJumpingDirection;
+    float wallJumpingTime = 0.2f;
+    float wallJumpingCounter;
+    float wallJumpingDuration = 0.4f;
 
     void Start()
     {
@@ -36,8 +54,16 @@ public class CharacterMovementComplete : MonoBehaviour
     void Update()
     {
         MovePlayerComplete();
+
         JumpPlayerComplete();
-        FlipCompleteCharacter();
+        WallSlide();
+        WallJump();
+
+        if (!iswallJumping)
+        {
+            FlipCompleteCharacter();
+        }
+
         Swinging();
     }
 
@@ -47,14 +73,24 @@ public class CharacterMovementComplete : MonoBehaviour
         yInputValue = Input.GetAxis("Vertical");
 
         Vector2 playerCompleteVelocity = new Vector2(xInputValue * playerSpeed, playerCompleteRigidody.velocity.y);
-        playerCompleteRigidody.velocity = playerCompleteVelocity;
+
+        if (!iswallJumping)
+        {
+            playerCompleteRigidody.velocity = playerCompleteVelocity;
+        }
+
 
         bool isMovingHorizontal = Mathf.Abs(playerCompleteRigidody.velocity.x) > Mathf.Epsilon;
 
-        if (playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) || playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Wall")))
         {
             playerCompleteAnimator.SetBool("isWalking", isMovingHorizontal);
             playerCompleteAnimator.SetFloat("walkSpeed", walkAnimationSpeed);
+        }
+        else
+        {
+            playerCompleteAnimator.SetBool("isWalking", isMovingHorizontal);
+            playerCompleteAnimator.SetFloat("walkSpeed", 0f);
         }
     }
 
@@ -82,13 +118,70 @@ public class CharacterMovementComplete : MonoBehaviour
         }
     }
 
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    void WallSlide()
+    {
+        bool isGrounded = playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        if (IsWalled() && !isGrounded && xInputValue != 0)
+        {
+            isWallSliding = true;
+            playerCompleteRigidody.velocity = new Vector2(playerCompleteRigidody.velocity.x, Mathf.Clamp(playerCompleteRigidody.velocity.y, -wallSlidngSpeed, float.MaxValue));
+            playerCompleteAnimator.SetBool("isWalking", false);
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    void WallJump()
+    {
+        if (isWallSliding)
+        {
+            iswallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f)
+        {
+            iswallJumping = true;
+            playerCompleteRigidody.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                FlipCompleteCharacter();
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    void StopWallJumping()
+    {
+        iswallJumping = false;
+    }
+
     void JumpPlayerComplete()
     {
-        /*if (playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        /*if (playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) || playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Wall")))
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 playerCompleteRigidody.velocity += new Vector2(0f, jumpSpeed);
+                //Add jump animation when done
             }
         }
 
@@ -97,24 +190,24 @@ public class CharacterMovementComplete : MonoBehaviour
             return;
         }*/
 
-        if (playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) || playerCompleteFeetCollider.IsTouchingLayers(LayerMask.GetMask("Wall")))
         {
             hasJumped = false;
-            hasWallJumped = false; 
+            hasWallJumped = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (!hasJumped)
             {
-                playerCompleteRigidody.velocity = new Vector2(0, firstJumpForce);
+                playerCompleteRigidody.velocity = new Vector2(0, jumpSpeed);
                 hasJumped = true;
-                hasWallJumped = false; 
+                hasWallJumped = false;
             }
-            else if (playerCompleteCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) && !hasWallJumped && playerCompleteCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Wall")))
+            else if (playerCompleteCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) && !hasWallJumped)
             {
-                playerCompleteRigidody.velocity = new Vector2(0, secondJumpForce);
-                hasWallJumped = true; 
+                playerCompleteRigidody.velocity = new Vector2(secondJumpPower.x, secondJumpPower.y);
+                hasWallJumped = true;
             }
         }
 
